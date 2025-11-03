119a403879c4f26b520f0ee7197c009c1f8a9b1f
Fernando1981-fern <gnonefernando@gmail.com>
2025-11-03 15:03:52 -0500

    Add a new branch

--- example.py
+++ example.py
@@ -1,2 +1,16 @@
-print('Hello, World!')
-print('Hi there!')
+import random
+
+greetings = [
+    "Hello, Git!",
+    "Greetings, developer!",
+    "Welcome to branching!",
+    "Hi there, coding friend!",
+    "Happy coding!"
+]
+
+def get_random_greeting():
+    return random.choice(greetings)
+
+print(get_random_greeting())
+print("Learning about branches today!")
+print("This is an additional line in the new branch.")
